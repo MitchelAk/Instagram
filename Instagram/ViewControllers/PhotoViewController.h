decd0badee393515de93047a4dfad39d5c285318
Mitchel Igolimah <mitcheligolimah@fb.com>
2022-06-29 09:54:09 -0700

    Posting photos

---
+++ Instagram/ViewControllers/PhotoViewController.h
@@ -0,0 +1,17 @@
+//
+//  PhotoViewController.h
+//  Instagram
+//
+//  Created by Mitchel Igolimah on 6/28/22.
+//
+
+#import <UIKit/UIKit.h>
+#import "Parse/Parse.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Instagram/ViewControllers/PhotoViewController.m
@@ -0,0 +1,104 @@
+//
+//  PhotoViewController.m
+//  Instagram
+//
+//  Created by Mitchel Igolimah on 6/28/22.
+//
+
+#import "PhotoViewController.h"
+#import "Parse/Parse.h"
+#import "Post.h"
+
+@interface PhotoViewController ()
+@property (strong, nonatomic)UIImage *postimage;
+@property (weak, nonatomic) IBOutlet UIImageView *photoImageView;
+
+@end
+
+@implementation PhotoViewController 
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+
+    // Do something with the images (based on your use case)
+    UIImage *resizedImage = [self resizeImage:originalImage withSize:CGSizeMake(200, 200)];
+        self.photoImageView.image = resizedImage;
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)ImageViewDidTap:(UITapGestureRecognizer *)sender {
+    NSLog(@"Picture tapped");
+    
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+
+    // The Xcode simulator does not support taking pictures, so let's first check that the camera is indeed supported on the device before trying to present it.
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+        
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+- (IBAction)didTapShare:(id)sender {
+    Post *post = [Post new];
+        PFFileObject *imageFile = [PFFileObject fileObjectWithName:@"image.png" data:UIImagePNGRepresentation(self.photoImageView.image)];
+        post.image = imageFile;
+//        post.caption = self.captionTextFeild.text;
+        
+        [post saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+            if(error != nil){
+                NSLog(@"User share failed: %@", error.localizedDescription);
+               
+            } else {
+                NSLog(@"User successfully shared post");
+                [self dismissViewControllerAnimated:YES completion:nil];
+            }
+        }];
+    
+}
+
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
